ca24575564350570881db0fe1fabdc56b119c770
Bret Short <bsshort@LAP1890.local>
2020-11-13 13:34:44 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,18 +73,58 @@ console.log(enemyName.length);
 //  * Defeat each enemy-robot
 // "LOSE" - PLayer robot's health is zero or less
 
-for (var i = 0; i < enemyName.length; i++) {
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1 ) );
-        var pickedEnemyName = enemyName[i];
-        enemyHealth = 50
-        //debugger;
+// function to start a new game
+var startGame = function() {
+    // reset player health stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    console.log("test start game function")
+    for (var i = 0; i < enemyName.length; i++) {
+        if (playerHealth > 0) {
+          window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+          var pickedEnemyName = enemyName[i];
+    
+          enemyHealth = 50;
+    
+          fight(pickedEnemyName);
+        }
+        else {
+          window.alert("You have lost your robot in battle! Game Over!");
+          break;
+        }
+    }
+      // play again
+    endGame();
+};
+
+//function to end the entire game
+var endGame = function() {
+    //if player is still alive, player wins!
+    if (playerName > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
     else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        window.alert("You've lost your robot in battle. ");
     }
-  var pickedEnemyName = enemyName[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
-}
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+    }
+    //window.alert("The game has now ended. Let's see how you did!");
+};
+
+
+  //var pickedEnemyName = enemyName[i];
+  //enemyHealth = 50;
+  //fight(pickedEnemyName);
+
+  // start the game when the page loads
+  startGame();
